7a0b76c6e13c99cc65b5d620557c02db49fc72ba
Kavernikolo <pagos1988@gmail.com>
2024-03-22 00:02:56 +0100

    Initial

---
+++ damasrv/src/main/java/com/dama/DamasrvApplication.java
@@ -0,0 +1,13 @@
+package com.dama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamasrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamasrvApplication.class, args);
+	}
+
+}
---
+++ damasrv/src/test/java/com/dama/DamasrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamasrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
